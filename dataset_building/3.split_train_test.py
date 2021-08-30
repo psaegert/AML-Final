a77c9e2b7560847d89750b1105373ce10a466c46
psaegert <paulsaegert@protonmail.com>
2021-08-30 16:48:05 +0200

    fixed data file extension

--- dataset_building/3.split_train_test.py
+++ dataset_building/3.split_train_test.py
@@ -15,8 +15,8 @@ del Z
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20210927)
 
-with open('../data/data_train.csv', 'wb') as file:
+with open('../data/data_train.pt', 'wb') as file:
     pickle.dump((X_train, y_train), file)
 
-with open('../data/data_test.csv', 'wb') as file:
+with open('../data/data_test.pt', 'wb') as file:
     pickle.dump((X_test, y_test), file)
