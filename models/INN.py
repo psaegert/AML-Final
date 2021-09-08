63394ea049ab98908af3b2294f5e97f2f1204cb0
psaegert <paulsaegert@protonmail.com>
2021-09-08 11:09:34 +0200

    potential fix for nan values: use one nn for both logS and T

--- models/INN.py
+++ models/INN.py
@@ -1,24 +1,37 @@
 import torch
+from torch._C import Value
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.modules.container import ModuleList
 
-class SkipConnectionNet(nn.Module):
+class NN(nn.Module):
     def __init__(self, in_features, hidden_layer_sizes, out_features):
-        super(SkipConnectionNet, self).__init__()
-        layers = []
+        super(NN, self).__init__()
+        self.layers = nn.ModuleList()
 
         n_in = in_features
         for n_out in hidden_layer_sizes:
-            layers.append(nn.Linear(n_in, n_out))
-            layers.append(nn.ReLU())
+            self.layers.append(nn.Linear(n_in, n_out))
+            self.layers.append(nn.ReLU())
             n_in = n_out
 
-        layers.append(nn.Linear(n_in, out_features))
+        self.logS_layers = ModuleList()
+        self.logS_layers.append(nn.Linear(hidden_layer_sizes[-1], out_features))
+        self.logS_layers.append(nn.Tanh())
 
-        self.model = nn.Sequential(*layers)
+        self.T_layers = ModuleList()
+        self.T_layers.append(nn.Linear(hidden_layer_sizes[-1], out_features))
 
     def forward(self, X):
-        return self.model(X)
+        for layer in self.layers:
+            X = layer(X)
+        
+        logS = self.logS_layers[0](X)
+        logS = self.logS_layers[1](logS)
+        
+        T = self.T_layers[0](X)
+
+        return logS, T
 
 
 class TwoWayAffineCoupling(nn.Module):
@@ -30,11 +43,8 @@ class TwoWayAffineCoupling(nn.Module):
         self.split_size_A = features // 2
         self.split_size_B = self.features - self.split_size_A
 
-        self.logSa = SkipConnectionNet(self.split_size_A, [32, 32], self.split_size_B)
-        self.Ta = SkipConnectionNet(self.split_size_A, [32, 32], self.split_size_B)
-
-        self.logSb = SkipConnectionNet(self.split_size_B, [32, 32], self.split_size_A)
-        self.Tb = SkipConnectionNet(self.split_size_B, [32, 32], self.split_size_A)
+        self.NNa = NN(self.split_size_A, [32, 32], self.split_size_B)
+        self.NNb = NN(self.split_size_B, [32, 32], self.split_size_A)
 
 
     def forward(self, input):
@@ -44,12 +54,28 @@ class TwoWayAffineCoupling(nn.Module):
         '''
         Xa, Xb = torch.chunk(input, 2, dim=1)
         
-        log_Sa = self.logSa(Xa)
-        log_Sb = self.logSb(Xb)
-        Za = Xa * torch.exp(log_Sb) + self.Tb(Xb)
-        Zb = Xb * torch.exp(log_Sa) + self.Ta(Xa)
+        log_Sa, Ta = self.NNa(Xa)
+        log_Sb, Tb = self.NNb(Xb)
+        Za = Xa * torch.exp(log_Sb) + Tb
+        Zb = Xb * torch.exp(log_Sa) + Ta
 
         self.logdet = log_Sa.sum(-1) + log_Sb.sum(-1)
+
+        if torch.isnan(Za).any() or torch.isnan(Zb).any():
+            print(f'{torch.where(torch.isnan(Xa)) = }')
+            print(f'{torch.where(torch.isnan(Xb)) = }')
+            print(f'{torch.min(Xb)=}')
+            print(f'{torch.max(Xb)=}')
+            
+            print(f'{torch.where(torch.isnan(log_Sa)) = }')
+            print(f'{torch.where(torch.isnan(log_Sb)) = }')
+
+            print(f'{torch.where(torch.isnan(self.Ta(Xa))) = }')
+            print(f'{torch.where(torch.isnan(self.Tb(Xb))) = }')
+            
+            print(f'{torch.where(torch.isnan(Za)) = }')
+            print(f'{torch.where(torch.isnan(Zb)) = }')
+            raise ValueError
 
         return torch.cat([Za, Zb], dim=1)
 
@@ -58,10 +84,17 @@ class TwoWayAffineCoupling(nn.Module):
         Xb = (Zb - Ta(Za)) / Sa(Za)
         Xa = (Za - Tb(Zb)) / Sb(Zb)
         '''
+
+        if torch.isnan(input).any():
+            print(f'{input = }')
+            raise ValueError
+
         Za, Zb = torch.chunk(input, 2, dim=1)
 
-        Xb = (Zb - self.Ta(Za)) * torch.exp(- self.logSa(Za))
-        Xa = (Za - self.Tb(Zb)) * torch.exp(- self.logSb(Zb))
+        log_Sa, Ta = self.NNa(Za)
+        log_Sb, Tb = self.NNb(Zb)
+        Xb = (Zb - Ta) * torch.exp(-log_Sa)
+        Xa = (Za - Tb) * torch.exp(-log_Sb)
 
         return torch.cat([Xa, Xb], dim=1)
 
@@ -97,10 +130,10 @@ class INN(nn.Module):
         self.layers = nn.ModuleList()
 
         for _ in range(n_blocks):
-            self.layers.append(RandomPermute(self.in_features, device=self.device))
+            # self.layers.append(RandomPermute(self.in_features, device=self.device))
             self.layers.append(TwoWayAffineCoupling(self.in_features, device=self.device))
         
-        self.layers.append(RandomPermute(self.in_features, device=self.device))
+        # self.layers.append(RandomPermute(self.in_features, device=self.device))
 
 
     def forward(self, X):
@@ -108,6 +141,10 @@ class INN(nn.Module):
 
         for layer in self.layers:
             X = layer.forward(X)
+            if torch.isnan(X).any():
+                print(layer._get_name())
+                print(torch.where(torch.isnan(X)))
+                raise ValueError
             if hasattr(layer, 'logdet'):
                 self.logdet_sum += layer.logdet
 
@@ -115,6 +152,10 @@ class INN(nn.Module):
 
 
     def inverse(self, Y, Z):
+        if torch.isnan(Y).any():
+            print(f'{Y = }')
+            raise ValueError
+
         YZ = torch.cat([Y, Z], dim=1)
         for layer in self.layers[::-1]:
             YZ = layer.inverse(YZ)
