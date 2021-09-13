cbc240de1dc24856b22d8b563a24bd8642b6d961
Daniel Schaeffer <daniel.schaeffer@stud.uni-heidelberg.de>
2021-09-14 01:34:23 +0200

    Update 2.combine_with_county_data.py

--- dataset_building/2.combine_with_county_data.py
+++ dataset_building/2.combine_with_county_data.py
@@ -12,6 +12,12 @@ education = pd.read_csv('../data/county_data/Education.csv', encoding = "latin")
 population = pd.read_csv('../data/county_data/PopulationEstimates.csv', encoding = "latin")
 unemployment = pd.read_csv('../data/county_data/Unemployment.csv', encoding = "latin")
 poverty = pd.read_csv('../data/county_data/PovertyEstimates.csv', encoding = "latin")
+voters = pd.read_csv('../data/county_data/countypres_2000-2020.csv', encoding = "latin")
+
+print('Reducing Voters Data...')
+voters = voters[voters['year'] == 2020]
+voters = voters[voters['county_fips'].notna()]
+voters = voters.reset_index(drop=True)
 
 print('Sorting Data by FIPS Code...')
 # sort data
@@ -20,12 +26,12 @@ education = education.sort_values("FIPS Code")
 population = population.sort_values("FIPStxt")
 unemployment = unemployment.sort_values("FIPS_Code")
 poverty = poverty.sort_values("FIPStxt")
+voters = voters.sort_values("county_fips")
 
 def fips_lookup(fips_column, current_index, fips, data):
     for idx, fipsi in enumerate(fips_column[current_index:]):
         if fipsi == fips:
             return data[idx+current_index]
-    print("not found")
     return "NaN"
 
 def fips_lookup2(column, fips, type_column, type_name, data):
@@ -34,9 +40,32 @@ def fips_lookup2(column, fips, type_column, type_name, data):
             for i in range(len(type_column[idx:])):
                 if type_column[idx+i] == type_name:
                     return data[idx+i]
-    print("not found")
     return "NaN"
 
+def fips_lookup3(df, column, fips, type_column, type_column2, type_name, type_name2, data1, data2):
+    for idx, fipsi in enumerate(column):
+        if (fipsi == fips):
+            i = 0
+            while (df.iloc[idx+i]['county_fips'] == fips):
+                if ((df.iloc[idx+i][type_column] == type_name) & (df.iloc[idx+i][type_column2] == type_name2)):
+                    if((df.iloc[idx+i][data1] == "NaN") or (df.iloc[idx+i][data2] == "NaN")):
+                        return "NaN"
+                    ratio = df.iloc[idx+i][data1]/df.iloc[idx+i][data2]
+                    return ratio
+                i += 1
+            i = 0
+            count = 0
+            while df.iloc[idx+i]['county_fips'] == fips:
+                if df.iloc[idx+i][type_column] == type_name:
+                    count += df.iloc[idx+i][data1]
+                i += 1
+            i = 0
+            ratio = count/df.iloc[idx+i][data2]
+            if count == 0:
+                break
+            else:
+                return ratio
+    return float(0)
 
 print('Combining...')
 # perform combination of data sets (takes ~1 minute on Intel® Core™ i5-5300U CPU @ 2.30GHz × 4 )
@@ -55,6 +84,11 @@ empl1_list = [None] * len(data)
 empl2_list = [None] * len(data)
 empl3_list = [None] * len(data)
 pov1_list = [None] * len(data)
+vot1_list = [None] * len(data)
+vot2_list = [None] * len(data)
+vot3_list = [None] * len(data)
+vot4_list = [None] * len(data)
+vot5_list = [None] * len(data)
 
 for idx, fips in enumerate(tqdm(data["county_fips_code"])):
     current_fips = fips
@@ -62,7 +96,6 @@ for idx, fips in enumerate(tqdm(data["county_fips_code"])):
     # whenever fips changes, we cache the necessary census data
     if (current_fips != old_fips):
         
-        # print current_fips to keep track of progress
         pop1 = fips_lookup(population["FIPStxt"], current_index, fips, population["Rural-urban_Continuum Code_2013"])
         pop2 = fips_lookup(population["FIPStxt"], current_index, fips, population["Urban_Influence_Code_2013"])
         pop3 = fips_lookup(population["FIPStxt"], current_index, fips, population["Economic_typology_2015"])
@@ -78,6 +111,13 @@ for idx, fips in enumerate(tqdm(data["county_fips_code"])):
         empl3 = fips_lookup2(unemployment["FIPS_Code"], fips, unemployment["Attribute"], "Med_HH_Income_Percent_of_State_Total_2019", unemployment["Value"])
        
         pov1 = fips_lookup2(poverty["FIPStxt"], fips, poverty["Attribute"], "PCTPOVALL_2019", poverty["Value"])
+        
+        vot1 = fips_lookup3(voters, voters["county_fips"], fips, "party", "mode", "DEMOCRAT", "TOTAL", "candidatevotes", "totalvotes")
+        vot2 = fips_lookup3(voters, voters["county_fips"], fips, "party", "mode", "REPUBLICAN", "TOTAL", "candidatevotes", "totalvotes")
+        vot3 = fips_lookup3(voters, voters["county_fips"], fips, "party", "mode", "GREEN", "TOTAL", "candidatevotes", "totalvotes")
+        vot4 = fips_lookup3(voters, voters["county_fips"], fips, "party", "mode", "LIBERTARIAN", "TOTAL", "candidatevotes", "totalvotes")
+        vot5 = fips_lookup3(voters, voters["county_fips"], fips, "party", "mode", "OTHER", "TOTAL", "candidatevotes", "totalvotes")
+        
         current_index += 1
     
     pop1_list[idx] = pop1
@@ -92,6 +132,11 @@ for idx, fips in enumerate(tqdm(data["county_fips_code"])):
     empl2_list[idx] = empl2
     empl3_list[idx] = empl3
     pov1_list[idx] = pov1
+    vot1_list[idx] = vot1
+    vot2_list[idx] = vot2
+    vot3_list[idx] = vot3
+    vot4_list[idx] = vot4
+    vot5_list[idx] = vot5
 
     old_fips = current_fips
 
@@ -101,19 +146,35 @@ data["rural-urban_Continuum Code_2013"] = pop1_list
 data["urban_influence_code_2013"] = pop2_list
 data["economic_typology_2015"] = pop3_list
 data["pop_estimate_2019"] = pop4_list
-data["less_hs_pct"] = edu1_list
-data["hs_only_pct"] = edu2_list
-data["some_college_pct"] = edu3_list
-data["bachelor_higher_pct"] = edu4_list
+data["less_than_hs_ratio"] = edu1_list
+data["hs_only_ratio"] = edu2_list
+data["some_college_ratio"] = edu3_list
+data["bachelor_or_higher_ratio"] = edu4_list
 data["unempl_rate"] = empl1_list
 data["med_hh_income_19"] = empl2_list
 data["med_hh_income_pct_state_total_19"] = empl3_list
-data["poverty_pct_19"] = pov1_list
+data["poverty_19_ratio"] = pov1_list
+data["democrat_20_ratio"] = vot1_list
+data["republican_20_ratio"] = vot2_list
+data["green_20_ratio"] = vot3_list
+data["libertarian_20_ratio"] = vot4_list
+data["other_20_ratio"] = vot5_list
 
-print('Performing Sanity Check...')
-# lastly, perform sanity check
-if (data_length == len(data)):
-    print('Writing Dataset...')
-    data.to_csv('../data/data.csv', index=False)
-else:
-    print("size of data sets is different")
+print('Cleaning Data Frame...')
+data['sum'] = data.apply(lambda row: row.democrat_20_ratio 
+                                   + row.republican_20_ratio
+                                   + row.green_20_ratio
+                                   + row.libertarian_20_ratio
+                                   + row.other_20_ratio, axis=1)
+
+data = data[data['sum'] != 0]
+data = data[data['sum'].isna() == False]
+
+data = data.drop('sum', 1)
+data = data.reset_index(drop=True)
+
+deleted_count = data_length - len(data)
+
+print(f'Dataset reduced by {deleted_count} entries.')
+print('Writing Dataset...')
+data.to_csv('../data/data.csv', index=False)
